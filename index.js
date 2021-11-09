5b7325e61034e365ace5dea9548c6d6a98b8ce4e
Michael Lobman <michael.lobman@gmail.com>
2021-11-09 14:35:15 -0500

    commit tree 6b83860fb04e4e17562bcf32914826916b7b2a68

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// Add your code here
+function submitData(userName, userEmail){
+    const body = document.querySelector('body');
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+    .then(resp => resp.json())
+    .then(object => {
+        const id = object.id;
+        body.append(id)
+    })
+    .catch(object => {
+        body.append(object.message)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+};
+
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify(formData),
+};
+
+fetch('http://localhost:3000/dogs', configurationObject)
+.then(response => response.json())
+.then((object) => console.log(object))
